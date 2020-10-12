79c3d8464d3ccbbb4bdfa56f6d7d61c2636c964b
Patrick Hayo <ph-de@flsmidth.com>
2020-10-12 12:43:53 +0200

    changed variable syntax

--- network/nsg.tf
+++ network/nsg.tf
@@ -1,6 +1,6 @@
 #Create NSG for default Subnet
 resource "azurerm_network_security_group" "default-nsg" {
-  name                = "${var.vnet-default-nsg}"
+  name                = var.vnet-default-nsg
   location            = azurerm_resource_group.vnet-rg.location
   resource_group_name = azurerm_resource_group.vnet-rg.name
 
