cc11a85de005447bb33f39ba41bdfa526ba8b708
adminph-de <36226934+adminph-de@users.noreply.github.com>
2020-10-13 08:16:57 +0000

    changes

--- build/cluster.tf
+++ build/cluster.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "aks" {
 }
 ## Create the virtual network for an AKS cluster
 module "network" {
-  source              = "git@github.com:adminph-de/tf-fls-aks-cluster.git/build/modules/virtual_network?ref=virtual_network-v0.6.0"
+  source              = "git@github.com:adminph-de/tf-fls-aks-cluster.git/build/modules/virtual_network?ref=deploy-1.0"
   region              = "westeurope"
   resource_group_name = azurerm_resource_group.aks.name
   name                = "aks-shared-vnet-1-p"
@@ -23,7 +23,7 @@ module "network" {
 }
 ## Create the AKS cluster
 module "cluster" {
-  source              = "git@github.com:adminph-de/tf-fls-aks-cluster.git/build/modules/aks_cluster?ref=aks_cluster-v0.8.0"
+  source              = "git@github.com:adminph-de/tf-fls-aks-cluster.git/build/modules/aks_cluster?ref=deploy-1.0"
   region              = "westeurope"
   cluster_name        = "aks-shared-1-p"
   kubernetes_version  = "1.17.11"
@@ -35,7 +35,7 @@ module "cluster" {
 }
 ## Create the node pool
 module "node_pool" {
-  source         = "git@adminph-de/tf-fls-aks-cluster.git/build/modules/aks_node_pool?ref=aks_node_pool-v0.4.0"
+  source         = "git@github.com:adminph-de/tf-fls-aks-cluster.git/build/modules/aks_node_pool?ref=deploy-1.0"
   name           = "linuxakspool"
   kubernetes_version  = "1.17.11"
   aks_cluster_id = module.cluster.id
