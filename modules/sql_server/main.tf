535d2eba89eaaddefb9fd62dcb9815a64daf0e58
adminph-de <36226934+adminph-de@users.noreply.github.com>
2020-10-14 12:38:13 +0000

    modified settings

--- modules/sql_server/main.tf
+++ modules/sql_server/main.tf
@@ -1,14 +1,10 @@
 resource "azurerm_sql_server" "sql" {
-  lifecycle {
-    ignore_changes = [
-      node_taints,
-      node_count
-    ]
-  }
+
   name                         = var.name
   resource_group_name          = var.resource_group_name
   location                     = var.region
   version                      = var.sku
   administrator_login          = var.admin_login
   administrator_login_password = var.admin_password
+
 }
